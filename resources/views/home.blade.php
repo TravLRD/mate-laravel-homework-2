55f1b3ee8ebd1fe291f4dda81944dc235dbbeaaa
Viktor Tar <viktor.tar@travlrd.com>
2023-05-31 13:27:05 +0200

    fixing home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,81 +1,43 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'Edit Blog Post')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+@section('content')
+<!-- Hero Section -->
+<header class="hero">
+    <div class="container my-5">
+        <h1 class="h1">Welcome to Confectionery Delights</h1>
+        <p class="lead">Indulge in the sweet delights and remember the taste of the ice-cream, the smell of the cakes!</p>
+        <img class="mw-100" src="image.jpeg" alt="">
+    </div>
+</header>
 
-    <title>Confectionery Delights</title>
-</head>
+<!-- Main Section -->
+<main>
+    <div class="container my-5">
+        <!-- Cakes Section -->
+        <section id="cakes">
+            <h2 class="h3">Our Cakes</h2>
+            <p>Our cakes are made with love and passion. They are the perfect choice for your special occasions.</p>
+            <!-- Add a carousel or image grid here -->
+        </section>
 
-<body>
-    <!-- Navbar Section -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Confectionery Delights</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cakes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Ice-cream</a>
-                </li>
-                <!-- add more navigation items here -->
-            </ul>
-        </div>
-    </nav>
+        <!-- Ice-cream Section -->
+        <section id="ice-cream" class="mt-5">
+            <h2 class="h3">Our Ice-creams</h2>
+            <p>Our ice-creams are crafted from the finest ingredients. Experience the joy of ice-cream like never before.</p>
+            <!-- Add a carousel or image grid here -->
+        </section>
 
-    <!-- Hero Section -->
-    <header class="hero bg-primary text-white text-center">
-        <div class="container">
-            <h1 class="display-4">Welcome to Confectionery Delights</h1>
-            <p class="lead">Indulge in the sweet delights and remember the taste of the ice-cream, the smell of the cakes!</p>
-            <img src="image.jpeg" alt="">
-        </div>
-    </header>
+        <!-- Add more sections as required -->
+    </div>
+</main>
 
-    <!-- Main Section -->
-    <main>
-        <div class="container my-5">
-            <!-- Cakes Section -->
-            <section id="cakes">
-                <h2 class="display-4">Our Cakes</h2>
-                <p>Our cakes are made with love and passion. They are the perfect choice for your special occasions.</p>
-                <!-- Add a carousel or image grid here -->
-            </section>
+<!-- Footer Section -->
+<footer class="footer bg-dark text-white text-center py-3">
+    <div class="container">
+        <p>&copy; 2023 Confectionery Delights. All Rights Reserved.</p>
+    </div>
+</footer>
 
-            <!-- Ice-cream Section -->
-            <section id="ice-cream" class="mt-5">
-                <h2 class="display-4">Our Ice-creams</h2>
-                <p>Our ice-creams are crafted from the finest ingredients. Experience the joy of ice-cream like never before.</p>
-                <!-- Add a carousel or image grid here -->
-            </section>
-
-            <!-- Add more sections as required -->
-        </div>
-    </main>
-
-    <!-- Footer Section -->
-    <footer class="footer bg-dark text-white text-center py-3">
-        <div class="container">
-            <p>&copy; 2023 Confectionery Delights. All Rights Reserved.</p>
-        </div>
-    </footer>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+@endsection
